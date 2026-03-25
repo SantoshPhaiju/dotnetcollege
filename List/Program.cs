1f5251b0d5c9441d37a98c46ae412b98cf249938
SantoshPhaiju <santoshphaiju062@gmail.com>
2026-03-25 07:38:28 +0545

    Add NuGet support files for project restoration
    - Created List.csproj.nuget.g.props to define properties for NuGet package restoration.
    - Added List.csproj.nuget.g.targets for build targets related to NuGet.
    - Generated project.assets.json to manage project dependencies and assets.
    - Included project.nuget.cache to store cache information for NuGet packages.

---
+++ List/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> students = new List<string>();
+            System.Console.WriteLine($"Capacity: {students.Capacity}");
+            students.Add("Alisha");
+            students.Add("Saugat");
+            students.Add("Santosh");
+            students.Add("Santosh");
+            students.Add("Santosh");
+            students.Add("Santosh");
+            System.Console.WriteLine($"Capacity: {students.Capacity}");
+            foreach (string student in students)
+            {
+                System.Console.WriteLine(student);
+            }
+            System.Console.WriteLine("Total students: " + students.Count);
+            System.Console.WriteLine("Is Alisha in the list? " + students.Contains("Alisha"));
+            students.Remove("Saugat");
+            System.Console.WriteLine("After removing Saugat:");
+            foreach (string student in students)
+            {
+                System.Console.WriteLine(student);
+            }
+
+            System.Console.WriteLine($"Capacity: {students.Capacity}");
+        }
+    }
+}
