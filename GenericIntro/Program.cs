165b7157a66ed76621c61b8bc0a70931838add67
SantoshPhaiju <santoshphaiju062@gmail.com>
2026-03-25 07:11:10 +0545

    Add NuGet support files for project configuration and asset management
    - Created delegates.csproj.nuget.g.props to define NuGet properties and restore settings.
    - Added delegates.csproj.nuget.g.targets for build targets related to NuGet.
    - Introduced project.assets.json to manage project dependencies and assets.
    - Generated project.nuget.cache to store cache information for NuGet operations.

---
+++ GenericIntro/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace GenericIntro
+{
+    internal class Program
+    {
+
+        class Student<T>
+        {
+            public T Data;
+
+            public Student(T new_data)
+            {
+                this.Data = new_data;
+            }
+
+            public void DisplayData<K>(K extraData)
+            {
+                System.Console.WriteLine("The data is: " + Data);
+                System.Console.WriteLine("The extra data is: " + extraData);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Student<string> student1 = new Student<string>("Hello, I'm a string");
+            System.Console.WriteLine(student1.Data);
+
+            Student<int> student2 = new Student<int>(42);
+            System.Console.WriteLine(student2.Data);
+
+            student1.DisplayData<int>(100);
+            student2.DisplayData<string>("This is extra data for student2");
+        }
+    }
+}
---
+++ delegates/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace delegates
+{
+    internal class Program
+    {
+        delegate void GetDetails(String name);
+        
+        class DivyaGyanCollege
+        {
+            public static void FirstSemester(String name)
+            {
+                System.Console.WriteLine("Welcome to the first semester of Divya Gyan College, " + name + "!");
+            }
+
+            public static void SecondSemester(String name)
+            {
+                System.Console.WriteLine("Welcome to the second semester of Divya Gyan College, " + name + "!");
+            }
+
+            public static void ThirdSemester(String name)
+            {
+                System.Console.WriteLine("Welcome to the third semester of Divya Gyan College, " + name + "!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            GetDetails detailsDelegate;
+
+            detailsDelegate = DivyaGyanCollege.FirstSemester;
+            detailsDelegate("Alisha");
+
+            detailsDelegate = DivyaGyanCollege.SecondSemester;
+            detailsDelegate("Saugat");
+
+            detailsDelegate = DivyaGyanCollege.ThirdSemester;
+            detailsDelegate("Santosh");
+        }
+    }
+}
