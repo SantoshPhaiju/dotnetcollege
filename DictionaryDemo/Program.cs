75e858c91574af2f16c6177bf4cf36d22461830b
SantoshPhaiju <santoshphaiju062@gmail.com>
2026-03-26 07:58:09 +0545

    Add NuGet support files for DictionaryDemo project
    - Created DictionaryDemo.csproj.nuget.g.props to define NuGet properties and restore settings.
    - Added DictionaryDemo.csproj.nuget.g.targets for build targets related to NuGet.
    - Generated project.assets.json to manage project dependencies and assets.
    - Included project.nuget.cache to store cache information for NuGet packages.

---
+++ DictionaryDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+namespace DictionaryDemo
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                
+            // Creating a dictionary
+            Dictionary<int, string> sub = new Dictionary<int, string>
+            {
+                { 1, "C#" },
+                { 2, "Javascript" },
+                { 3, "Dart" },
+                { 3, "Python" } // This will overwrite the previous value for key 3
+            };
+
+            // Displaying dictionary
+            foreach (var ele in sub)
+            {
+                Console.WriteLine($"Key: {ele.Key}, Value: {ele.Value}");
+            }
+            } catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+        }
+    }
+}
